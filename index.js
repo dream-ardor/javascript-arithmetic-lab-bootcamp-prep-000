37ed2d57836b1646570bac3019924a674bada03f
Dan Barrick <barrick.dan80@gmail.com>
2018-08-26 04:37:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ var number;
 
 function add(a,b){
   number = a+b;
+  return number;
 }
-function subtract(a,b){}
-function multiply(a,b){}
-function divide(a,b){}
+function subtract(a,b){
+  number = a-b;
+  return number;
+}
+function multiply(a,b){
+  number = a*b;
+  return number;
+}
+function divide(a,b){
+  number = a/b;
+  return number;
+}
 
 function inc(n){}
 function dec(n){}
